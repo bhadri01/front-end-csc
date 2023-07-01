f5edb1c35ad715b5b2c4f16594c6fe348fc61f9d
nocturnalplay <nocturnalplay0@gmail.com>
2023-07-01 15:08:27 +0530

    day-10

---
+++ day-9/script.js
@@ -0,0 +1,149 @@
+const datalist = [
+  {
+    id: 1,
+    name: "John Smith",
+    age: 20,
+    gender: "male",
+    major: "Computer Science",
+    gpa: 3.8,
+    courses: [
+      "Introduction to Programming",
+      "Data Structures",
+      "Algorithms",
+      "Database Management",
+    ],
+  },
+  {
+    id: 2,
+    name: "Emily Johnson",
+    age: 21,
+    gender: "female",
+    major: "Biology",
+    gpa: 3.6,
+    courses: ["Genetics", "Anatomy", "Cell Biology", "Biochemistry"],
+  },
+  {
+    id: 3,
+    name: "John Smith",
+    age: 20,
+    gender: "male",
+    major: "Computer Science",
+    gpa: 3.8,
+    courses: [
+      "Introduction to Programming",
+      "Data Structures",
+      "Algorithms",
+      "Database Management",
+    ],
+  },
+  {
+    id: 4,
+    name: "Emily Johnson",
+    age: 21,
+    gender: "female",
+    major: "Biology",
+    gpa: 3.6,
+    courses: ["Genetics", "Anatomy", "Cell Biology", "Biochemistry"],
+  },
+  {
+    id: 5,
+    name: "John Smith",
+    age: 20,
+    gender: "male",
+    major: "Computer Science",
+    gpa: 3.8,
+    courses: [
+      "Introduction to Programming",
+      "Data Structures",
+      "Algorithms",
+      "Database Management",
+    ],
+  },
+  {
+    id: 6,
+    name: "Emily Johnson",
+    age: 21,
+    gender: "female",
+    major: "Biology",
+    gpa: 3.6,
+    courses: ["Genetics", "Anatomy", "Cell Biology", "Biochemistry"],
+  },
+  {
+    id: 7,
+    name: "John Smith",
+    age: 20,
+    gender: "male",
+    major: "Computer Science",
+    gpa: 3.8,
+    courses: [
+      "Introduction to Programming",
+      "Data Structures",
+      "Algorithms",
+      "Database Management",
+    ],
+  },
+  {
+    id: 8,
+    name: "Emily Johnson",
+    age: 21,
+    gender: "female",
+    major: "Biology",
+    gpa: 3.6,
+    courses: ["Genetics", "Anatomy", "Cell Biology", "Biochemistry"],
+  },
+];
+
+const classArollno = [12, 23, 45, 58, 56];
+const classAnames = ["sdfg", "3456", "sdfg", "asdfzxcv"];
+const dummy3 = [true, false];
+const dummy4 = [
+  {
+    name: "John Smith",
+    age: 20,
+    gender: "male",
+    major: "Computer Science",
+  },
+  {
+    name: "John",
+    age: 22,
+    gender: "male",
+    major: "Computer Science",
+  },
+];
+
+const dummy5 =  {
+    name: "John Smith",
+    age: 20,
+    gender: "male",
+    major: "Computer Science",
+  }
+
+const tdf = document.getElementById("tdf")
+
+document.write(dummy4[1].name);
+
+// for (var i = 0; i < classArollno.length; i++) {
+//     if(classArollno[i] >= 18)
+//     tdf.innerHTML += classArollno[i]+"--"
+// }
+
+// classArollno.map(function(data){
+//     tdf.innerHTML += data + "---"
+// })
+
+
+// classArollno.map((data,index)=>{
+//     tdf.innerHTML += data+"@"+index+"_"
+// })
+
+
+// function add(num1,num2){
+//     tdf.innerHTML = (num1+num2)+100
+// }
+
+// add(20,30)
+// add(30,50)
+// add(50,70)
+// add(20,30)
+// add(20,30)
+// add(20,30)
